b5c8836b34be83e07403aa3415070ae38651a653
mulloverit <camillamahon@fastmail.com>
2019-02-12 13:29:40 -0800

    simplifies routes to homepage, adds comments to existing routes

--- server.py
+++ server.py
@@ -4,7 +4,6 @@ from flask import Flask, request, jsonify, render_template
 from model import User, InputImage, DiffImage
 from config import *
 
-
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
 
@@ -14,41 +13,49 @@ def show_index():
 
         return render_template("index.html")
 
-@app.route("/signin", methods=['POST'])
+@app.route("/", methods=['POST'])
 def sign_in():
-    """Add user to database"""
+    """Log in an existing user"""
 
+    # Retrieve POSTed form data
     username = request.form['username']
     password = request.form['password']
 
+    # Check if username exists
     try:
         user = User.query.filter(User.username == username).one()
 
+        # If Y, check that password is valid
         if user.password == password:
 
             session['username'] = username
             flash("Successfully logged in")
 
-            return render_template("index.html")
-
+    # If N, notify user of failed login
     except:
-        return redirect("/register")
+        flash("Login failed")
 
-@app.route("/register-new", methods=['POST'])
+    return render_template("index.html")
+
+@app.route("/", methods=['POST'])
 def register_user():
-    """Add user to database"""
+    """Add a new user to database"""
 
+    # Retrieve POSTed form data
     username = request.form['username']
     password = request.form['password']
     email = request.form['email']
     fname = request.form['fname']
     lname = request.form['lname']
 
+    # Check to see if username already taken. 
     try:
+        # If Y, ask for unique username.
         user = User.query.filter(User.username == username).one()
         flash("Already a user. Please pick a unique username or sign in.")
 
     except:
+        # If N taken, add the new user to psql database table Users
         user = User(username=username,
                     password=password,
                     email=email,
@@ -57,6 +64,8 @@ def register_user():
 
         db.session.add(User)
         db.session.commit()
+
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
