45d8b79a1a5e922a845afcc575083de6809f7e5b
mulloverit <camillamahon@fastmail.com>
2019-02-12 15:11:31 -0800

    adds v basic InputImage class with sole attribte being the image's filepath, and sole method being a retrieval of basic image info. comments out spaghetti code while wip.

--- difflogic.py
+++ difflogic.py
@@ -7,9 +7,9 @@ import sys, os
 ############# SPAGHETTI CODE FOR INITAL IMAGE DIFF #######################
 
 try:
-
-    img_1_path = sys.argv[0]
-    img_2_path = sys.argv[1]
+    
+    img_1_path = sys.argv[1]
+    img_2_path = sys.argv[2]
 
 except:
 
@@ -17,23 +17,23 @@ except:
     img_1_path = input("Image 1: ") # 'test-fixtures/imgs/inputs/img_1.jpg'
     img_2_path = input("Image 2: ") # 'test-fixtures/imgs/inputs/img_2.jpg'
 
-img_1_basename = os.path.basename(img_1_path)
-img_2_basename = os.path.basename(img_2_path)
+# img_1_basename = os.path.basename(img_1_path)
+# img_2_basename = os.path.basename(img_2_path)
 
-img_1_suffix = os.path.splitext(img_1_basename)[0]
-img_2_suffix = os.path.splitext(img_2_basename)[0]
+# img_1_suffix = os.path.splitext(img_1_basename)[0]
+# img_2_suffix = os.path.splitext(img_2_basename)[0]
 
-img_1 = Image.open(img_1_path)
-img_2 = Image.open(img_2_path)
+# img_1 = Image.open(img_1_path)
+# img_2 = Image.open(img_2_path)
 
-diff = ImageChops.difference(img1, img2)
+# diff = ImageChops.difference(img1, img2)
 
-img1.close()
-img2.close()
+# img1.close()
+# img2.close()
 
-diff.show()
-diff.save('test-fixtures/imgs/diffs/diff_{}_{}.jpg'.format())
-diff.close()
+# diff.show()
+# diff.save('test-fixtures/imgs/diffs/diff_{}_{}.jpg'.format())
+# diff.close()
 
 
 
@@ -42,21 +42,38 @@ diff.close()
 # Image attributes: filepath 
 # Image methods: getinfo(size, method, format)
 
-# class Image:
+class InputImage:
 
-#     def __init__(self, filepath):
-#         self.filepath = filepath
-#         self.info()
+    def __init__(self, filepath):
+        """Instantiate an Image class object"""
+        self.filepath = filepath
 
-#     def get_info(self):
-        
-#         im_size = filepath.size
-#         im_mode = filepath.mode
-#         im_format = filepath.format
+    # method to retrieve basic information about an image
+    def get_info(self):
+        im = Image.open(self.filepath) # open file
 
-#         print(f"Filepath: {self.filepath}")
+        # retrieve data via PIL Image attributes
+        im_size = im.size
+        im_mode = im.mode
+        im_format = im.format
+
+        im.close() # responsibly close file
+
+        print(f"""\t\tFilepath: {self.filepath},
+                  Size: {im_size},
+                  Mode: {im_mode},
+                  Format: {im_format}""")
 
 
 ##########################################################
 
 
+#### FOR DOCTESTS LATER #####
+
+# img_1_path = '/Users/mulloverit/mulloverit-gh/hba-main-proj/test-fixtures/imgs/inputs/img_1.jpg'
+# im1 = InputImage(img_1_path)
+# im1.get_info()
+# >>>        Filepath: /Users/mulloverit/mulloverit-gh/hba-main-proj/test-fixtures/imgs/inputs/img_1.jpg,
+#                   Size: (2283, 2283),
+#                   Mode: RGB,
+#                   Format: JPEG
