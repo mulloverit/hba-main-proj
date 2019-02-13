52e41ffc788af06832cf30a8a2e4a57dd4f4f55a
mulloverit <camillamahon@fastmail.com>
2019-02-12 17:02:09 -0800

    adds logic to convert diff image to boolean output, making areas that have different between two input imgs more obvious

--- difflogic.py
+++ difflogic.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageChops # imagechops -> "image channel operations"
+from PIL import Image, ImageChops, ImageStat # imagechops -> "image channel operations"
 import sys, os
 
 from testseed import load_users, load_input_imgs, load_diff_imgs
@@ -31,7 +31,7 @@ class InputImage:
                   Format: {im_format}""")
 
 
-#################### Work: 
+#################### Make cheap pixel image difference: 
 
 try:
     img_1_path, img_2_path = [sys.argv[1], sys.argv[2]]
@@ -47,9 +47,35 @@ img_1, img_2 = InputImage(img_1_path), InputImage(img_2_path)
 diff_input_1, diff_input_2 = Image.open(img_1.filepath), Image.open(img_2.filepath)
 
 # Perform image differencing, save the diff
-diff = ImageChops.difference(diff_input_1, diff_input_2)
-diff.save('test-fixtures/imgs/diffs/diff_{}_{}.jpg'.format(img_1.filename, img_2.filename))
+diff_img = ImageChops.difference(diff_input_1, diff_input_2)
+diff_img.save('test-fixtures/imgs/diffs/diff_{}_{}.jpg'.format(img_1.filename, img_2.filename))
 
-# Close all image files
-diff_input_1.close(), diff_input_2.close(), diff.close()
+#################### Make boolean difference:
 
+# Convert input images to single channel, 8-bit, black and white images
+im1l, im2l = diff_input_1.convert("L"), diff_input_2.convert("L")
+bw_diff = ImageChops.difference(im1l, im2l) # compute their diff at every pixel
+bw_stat = ImageStat.Stat(bw_diff) # create an image statistics object
+bw_median = bw_stat.median # grab the diff's median value
+
+# Create a new image that will be populated by boolean image
+# Open a list for storing boolean values
+bool_img = Image.new("L", bw_diff.size)
+bool_vals = []
+
+# Change all values to either 0 or 255, split based on median val of diff
+for pixel in bw_diff.getdata():
+    if pixel > bw_median[0]:
+        bool_vals.append(255) # 255 instead of 1 cuz 8 bits
+    else:
+        bool_vals.append(0)
+
+# Add new vals to new image and save boolean image next to initial diff
+bool_img.putdata(bool_vals)
+bool_img.save('test-fixtures/imgs/diffs/diff_{}_{}_bool.jpg'.format(img_1.filename, img_2.filename))
+
+# For debugging:
+diff_img.show(), bool_img.show()
+
+# Close up shop
+diff_input_1.close(), diff_input_2.close(), diff_img.close(), bool_img.close()
