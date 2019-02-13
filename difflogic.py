d24bee4bb759dd3a28da4f5cc3bace76ea77d476
mulloverit <camillamahon@fastmail.com>
2019-02-12 16:15:57 -0800

    cleans up syntax and repetitive lines

--- difflogic.py
+++ difflogic.py
@@ -3,46 +3,9 @@ import sys, os
 
 from testseed import load_users, load_input_imgs, load_diff_imgs
 
-# useful Image attributes: .format, .size, .mode, .bits, .pallette
-# useful Image methods: .getbbox(), .histogram(), .putalpha(), .resize(), .rotate(), .crop(), .save(), .copy(), .open()
-
-############# SPAGHETTI CODE FOR INITAL IMAGE DIFF #######################
-
-try:
-    #input_images = [sys.argv[1], sys.argv[2]]
-    
-    img_1_path = sys.argv[1]
-    img_2_path = sys.argv[2]
-
-except:
-
-    print("Please provide image inputs.\n")
-    img_1_path = input("Image 1: ") # 'test-fixtures/imgs/inputs/img1.jpg'
-    img_2_path = input("Image 2: ") # 'test-fixtures/imgs/inputs/img2.jpg'
-
-# img_1_basename = os.path.basename(img_1_path)
-# img_2_basename = os.path.basename(img_2_path)
-
-# img_1_name = os.path.splitext(img_1_basename)[0]
-# img_2_name = os.path.splitext(img_2_basename)[0]
-
-# img_1 = Image.open(img_1_path)
-# img_2 = Image.open(img_2_path)
-
-# diff = ImageChops.difference(img_1, img_2)
-
-# img_1.close()
-# img_2.close()
-
-# diff.show()
-# diff.save('test-fixtures/imgs/diffs/diff_{}_{}.jpg'.format(img_1_name, img_2_name))
-# diff.close()
-
-#######################################################
-
-# Image attributes: filepath 
-# Image methods: getinfo(size, method, format)
-
+# useful Image attributes:  .format, .size, .mode, .bits, .pallette
+# useful Image methods:     .getbbox(), .histogram(), .putalpha(), .resize(), 
+#                           .rotate(), .crop(), .save(), .copy(), .open()
 
 class InputImage:
 
@@ -70,32 +33,25 @@ class InputImage:
                   Format: {im_format}""")
 
 
-
 ##########################################################
 
+try:
+    img_1_path, img_2_path = [sys.argv[1], sys.argv[2]]
+    
+except:
 
-img_1 = InputImage(img_1_path)
-img_2 = InputImage(img_2_path)
-diff_input_1 = Image.open(img_1.filepath)
-diff_input_2 = Image.open(img_2.filepath)
+    print("Please provide image inputs.\n")
+    img_1_path = input("Image 1: ") # 'test-fixtures/imgs/inputs/img1.jpg'
+    img_2_path = input("Image 2: ") # 'test-fixtures/imgs/inputs/img2.jpg'
 
+# Establish image classes and open files for differencing
+img_1, img_2 = InputImage(img_1_path), InputImage(img_2_path)
+diff_input_1, diff_input_2 = Image.open(img_1.filepath), Image.open(img_2.filepath)
+
+# Perform image differencing, save the diff
 diff = ImageChops.difference(diff_input_1, diff_input_2)
-
 diff.save('test-fixtures/imgs/diffs/diff_{}_{}.jpg'.format(img_1.filename, img_2.filename))
 
-diff.close()
+# Close all image files
+diff_input_1.close(), diff_input_2.close(), diff.close()
 
-
-
-##########################################################
-
-
-#### FOR DOCTESTS LATER #####
-
-# img_1_path = '/Users/mulloverit/mulloverit-gh/hba-main-proj/test-fixtures/imgs/inputs/img_1.jpg'
-# im1 = InputImage(img_1_path)
-# im1.get_info()
-# >>>        Filepath: /Users/mulloverit/mulloverit-gh/hba-main-proj/test-fixtures/imgs/inputs/img_1.jpg,
-#                   Size: (2283, 2283),
-#                   Mode: RGB,
-#                   Format: JPEG
