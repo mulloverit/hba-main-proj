9028da830e3c82476ba668a3ce501aaf95e1fc1e
mulloverit <camillamahon@fastmail.com>
2019-02-22 16:42:49 -0800

    have basic form post outlined for image upload, but not effectively hitting server yet

--- static/js/main.js
+++ static/js/main.js
@@ -5,7 +5,10 @@ class ImageManipulator extends React.Component {
     return (
       <div>
         <h1>Hey</h1>
+        <br />
         <SearchBar />
+        <br />
+        <ImageUploadForm />
       </div>
     );
   }
@@ -22,8 +25,33 @@ class SearchBar extends React.Component {
 }
 
 
+// form not working yet
+// need to read: https://reactjs.org/docs/forms.html
+class ImageUploadForm extends React.Component {
 
+  handleSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    
+    fetch('/upload-inputs', {
+      method: 'POST',
+      body: data,
+    })
+    .then((response) => {response.json()});
+  }
 
+  render() {
+    return (
+      <div>
+        <form action="/upload-inputs" className="user-input-images-form" method="POST" encType="multipart/form-data">
+          <input type="file" name="img-1" id="img-1" />
+          <input type="file" name="img-2" id="img-2" />
+          <input type="submit" name="upload-inputs" value="Upload" id="user-image-submission"></input>
+        </form>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <ImageManipulator />,
