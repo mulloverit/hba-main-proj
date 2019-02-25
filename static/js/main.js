c28b2becb95e535124de2ebeea1edcd34538b31e
mulloverit <camillamahon@fastmail.com>
2019-02-25 12:42:59 -0800

    IF this were a regular input form (not file based), current onChange and handleSubmit is how we would accomplish state change. HOWEVER, will need to change this, since file submissions are not 'controlled components' the way other inputs are... they are 'uncontrolled components' --> https://reactjs.org/docs/uncontrolled-components.html#the-file-input-tag

--- static/js/main.js
+++ static/js/main.js
@@ -27,7 +27,20 @@ class SearchBar extends React.Component {
 
 // form not working yet
 // need to read: https://reactjs.org/docs/forms.html
+// IF THIS WERE A CONTROLLED COMPONENT, following changes:
+// however, image uploads are UNCONTROLLED COMPONENTS 
 class ImageUploadForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+  this.handleChange = this.handleChange.bind(this);
+  this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
 
   handleSubmit(event) {
     event.preventDefault();
@@ -44,8 +57,8 @@ class ImageUploadForm extends React.Component {
     return (
       <div>
         <form action="/upload-inputs" className="user-input-images-form" method="POST" encType="multipart/form-data">
-          <input type="file" name="img-1" id="img-1" />
-          <input type="file" name="img-2" id="img-2" />
+          <input type="file" name="img-1" id="img-1" value={this.state.value} onChange={this.handleChange} />
+          <input type="file" name="img-2" id="img-2" value={this.state.value} onChange={this.handleChange} />
           <input type="submit" name="upload-inputs" value="Upload" id="user-image-submission"></input>
         </form>
       </div>
