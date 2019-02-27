d63191babb269bcb83c19d4a836ad7099845a400
mulloverit <camillamahon@fastmail.com>
2019-02-26 17:13:19 -0800

    intial drag and drop working on image display containers

--- static/js/main.js
+++ static/js/main.js
@@ -1,38 +1,78 @@
 "use strict";
 
-FilePond.setOptions({
-  server: {
-    url: '/upload-inputs',
-  }
-})
+var Draggable = window.ReactDraggable;
 
-const inputElement = document.querySelector('input[type="file"]');
-const pond = FilePond.create( inputElement );
+
+class DraggableDefinition extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleDrag = this.handleDrag.bind(this);
+    this.onStart = this.onStart.bind(this);
+    this.onStop = this.onStop.bind(this);
+
+
+    this.state = {
+      activeDrags: 0,
+      deltaPosition: {
+        x: 0, y: 0
+      },
+      controlledPosition: {
+        x: -400, y: 200
+      }
+    }
+  }
+
+  handleDrag(event, ui) {
+    const {x, y} = this.state.deltaPosition;
+    this.setState({
+      deltaPosition: {
+        x: x + ui.deltaX,
+        y: y + ui.deltaY
+      }
+    });
+  }
+
+  onStart() {
+    this.setState({
+      activeDrags: ++this.state.activeDrags
+    });
+  }
+
+  onStop() {
+    this.setState({
+      activeDrags: --this.state.activeDrags
+    });
+  }
+
+
+  render() {
+    const dragHandlers = {onStart: this.onStart, onStop: this.onStop};
+    const {deltaPosition, controlledPosition} = this.state;
+  
+    return (
+      <Draggable {...dragHandlers}>
+        <div className="container-display">. I can be dragged anywhere</div>
+      </Draggable>
+    );
+  }
+}
 
 class DynamicGreeting extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: 'guest'
+    }
+  }
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-6">
             <br />
-            <h1>Hey</h1>
-            <br />
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
-
-class ImageUploader extends React.Component {
-  render () {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-6">
-            <br />
-            <ImageUploadForm />
+            <h1>Welcome, {this.state.name} !</h1>
             <br />
           </div>
         </div>
@@ -51,7 +91,9 @@ class ImageUploadForm extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    
+
+    const imageContainerOne = document.querySelector('#image-display-container #contained-image');
+    const imageContainerTwo = document.getElementById('#image-display-container-2 #contained-image');
     const formData = new FormData();
     const fileOne = new File(this.fileInputOne.current.files, this.fileInputOne.current.files.name);
     const fileTwo = new File(this.fileInputTwo.current.files, this.fileInputOne.current.files.name);
@@ -61,7 +103,15 @@ class ImageUploadForm extends React.Component {
     formData.append('file1', fileOne);
     formData.append('file2', fileTwo);
     xmlPackage.open("POST", postUrl);
-    xmlPackage.send(formData)
+    xmlPackage.responseType = 'json';
+    xmlPackage.onload = function() {
+      const imageUrls = xmlPackage.response;
+      imageContainerOne.setAttribute('src', imageUrls[0]);
+      imageContainerTwo.setAttribute('src', imageUrls[1]);
+    }
+
+    xmlPackage.send(formData);
+
   }
   render() {
     return (
@@ -80,6 +130,22 @@ class ImageUploadForm extends React.Component {
   }
 }
 
+class ImageDisplayContainer extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <div className="container-display">
+              <img id="contained-image" src="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+}
+
 
 ReactDOM.render(
   <DynamicGreeting />,
@@ -87,9 +153,27 @@ ReactDOM.render(
 );
 
 ReactDOM.render(
-  <ImageUploader />,
-  document.getElementById('manual-uploads')
+  <ImageDisplayContainer />,
+  document.getElementById('image-display-container')
+);
+
+ReactDOM.render(
+  <ImageDisplayContainer />,
+  document.getElementById('image-display-container-2')
+);
+
+ReactDOM.render(
+  <DraggableDefinition />,
+  document.getElementById('image-display-container')
+);
+
+ReactDOM.render(
+  <DraggableDefinition />,
+  document.getElementById('image-display-container-2')
 );
 
 
-
+ReactDOM.render(
+  <ImageUploadForm />,
+  document.getElementById('image-upload-form')
+);
